fd1e9feaed7d9de0ef0baec3898f9fed76f8b3e7
fbottarel <fabrizio.bottarel@altorobotics.ai>
2025-01-22 15:05:24 +0000

    formatting for consistency

--- src/pointcloud_to_laserscan_node.cpp
+++ src/pointcloud_to_laserscan_node.cpp
@@ -75,9 +75,12 @@ PointCloudToLaserScanNode::PointCloudToLaserScanNode(const rclcpp::NodeOptions &
   inf_epsilon_ = this->declare_parameter("inf_epsilon", 1.0);
   use_inf_ = this->declare_parameter("use_inf", true);
 
-  auto exclusion_box_x_param_ = this->declare_parameter<std::vector<double>>("exclude_within_x", {0.0, 0.0});
-  auto exclusion_box_y_param_ = this->declare_parameter<std::vector<double>>("exclude_within_y", {0.0, 0.0});
-  auto exclusion_box_z_param_ = this->declare_parameter<std::vector<double>>("exclude_within_z", {0.0, 0.0});
+  auto exclusion_box_x_param_ = this->declare_parameter<std::vector<double>>(
+    "exclude_within_x", {0.0, 0.0});
+  auto exclusion_box_y_param_ = this->declare_parameter<std::vector<double>>(
+    "exclude_within_y", {0.0, 0.0});
+  auto exclusion_box_z_param_ = this->declare_parameter<std::vector<double>>(
+    "exclude_within_z", {0.0, 0.0});
 
   if ((exclusion_box_x_param_[1] >= exclusion_box_x_param_[0]) &&
     (exclusion_box_y_param_[1] >= exclusion_box_y_param_[0]) &&
@@ -89,9 +92,7 @@ PointCloudToLaserScanNode::PointCloudToLaserScanNode(const rclcpp::NodeOptions &
     exclusion_box_max_y_ = exclusion_box_y_param_[1];
     exclusion_box_min_z_ = exclusion_box_z_param_[0];
     exclusion_box_max_z_ = exclusion_box_z_param_[1];
-  }
-  else
-  {
+  } else {
     exclusion_box_min_x_ = 0.0;
     exclusion_box_max_x_ = 0.0;
     exclusion_box_min_y_ = 0.0;
@@ -226,9 +227,9 @@ void PointCloudToLaserScanNode::cloudCallback(
       (*iter_z > exclusion_box_min_z_ && *iter_z < exclusion_box_max_z_)) {
       RCLCPP_DEBUG(
         this->get_logger(),
-        "rejected for point (%f, %f, %f) being in the exclusion region x: (%f, %f) y: (%f, %f), z: (%f, %f)\n",
-        *iter_x, *iter_y, *iter_z, exclusion_box_min_x_, exclusion_box_max_x_, exclusion_box_min_y_,
-        exclusion_box_max_y_, exclusion_box_min_z_, exclusion_box_max_z_);
+        "rejected for point (%f, %f, %f) being in the exclusion region x: (%f, %f) y: (%f, %f)"
+        "z: (%f, %f)\n", *iter_x, *iter_y, *iter_z, exclusion_box_min_x_, exclusion_box_max_x_,
+        exclusion_box_min_y_, exclusion_box_max_y_, exclusion_box_min_z_, exclusion_box_max_z_);
       continue;
     }
 
