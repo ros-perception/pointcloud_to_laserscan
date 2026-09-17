e921eb9f4ed9893ac491333a4b6cbf42a26dc97b
Intrepid <none>
2026-09-17 16:49:23 +0000

    fix build issue in jazzy

--- src/pointcloud_to_laserscan_node.cpp
+++ src/pointcloud_to_laserscan_node.cpp
@@ -127,7 +127,7 @@ void PointCloudToLaserScanNode::subscriptionListenerThreadLoop()
           "Got a subscriber to laserscan, starting pointcloud subscriber");
         rclcpp::SensorDataQoS qos;
         qos.keep_last(input_queue_size_);
-        sub_.subscribe(this, "cloud_in", qos);
+        sub_.subscribe(this, "cloud_in", qos.get_rmw_qos_profile());
       }
     } else if (sub_.getSubscriber()) {
       RCLCPP_INFO(
